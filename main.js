0429ff89635087cd8084ddd2bba6260596c182c0
jerome delfosse <jeromedelfosse@macbook-pro-de-jerome.home>
2018-09-25 22:33:22 +0200

    ajout du site v1

---
+++ main.js
@@ -0,0 +1,7 @@
+function popup()
+{
+	if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'))
+	document.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
+setTimeout(popup, 10000);
+
